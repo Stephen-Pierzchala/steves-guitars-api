bf2115276afa65693b47510b529a91090e849bf8
Stephen Pierzchala <pierzcha@usc.edu>
2021-01-21 13:53:26 -0600

    bugfix, imported wrong controller

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const cartController = require("../controllers/authentication/auth");
+const cartController = require("../controllers/cartItem/cartItem");
 
 router.get("", (req, res) => {
 	res.json({ status: "Working!" });
